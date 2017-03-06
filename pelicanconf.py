1fad6cc62bee91fc14a65ea4a302964b3bbb3aa2
unknown <>
2017-03-07 00:36:02 +0800

    40423143

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = '2016Fall 電腦輔助設計實習 (虎尾科大MDE)'
+AUTHOR = '40423143'
+SITENAME = '協同產品設計實習'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
